ed33027f11b0ba04a6559cf0e6896d07a0fae12a
Bamba <cabkebe02@gmail.com>
2022-10-14 23:14:53 +0000

    requêtes avancées: filtrer les requêtes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 
 const app = express()
-const port = 3000
+const port = 3948
 
 
 app
--- src/routes/findAllpokemons.js
+++ src/routes/findAllpokemons.js
@@ -1,15 +1,34 @@
 const { Pokemon } = require('../db/sequelize')
-  
+const {Op} = require('sequelize') //Operateur de sequelize qui permet de fairedes recherche plus complexe
+
+
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
-      .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
+    if (req.query.name) {
+      const name = req.query.name
+      return pokemon.findAll({
+        where : {
+          name : { // 'name' est la propriété du model pokemons
+            [Op.like]: `%${name}%` //name est la critère de recherche 
+            //et on fait de sort que le resultat fournit aie une/plusieurs lettre du mot fournit lors de la recherche
+          }
+        }
       })
-      .catch(error => {
-        const message = `La liste des pokemons n'a pas pu etre trouvé. Réessayer dans quelques instants`
-        res.status(500).json({message, data: error})
+      .then (pokemon => {
+        const message = `il y a ${pokemons.lenght} pokémons qui correspondent aux termes recherché: ${name}.`
+        res.json({message, data: pokemons})
       })
+    }
+    else{
+      Pokemon.findAll()
+        .then(pokemons => {
+          const message = 'La liste des pokémons a bien été récupérée.'
+          res.json({ message, data: pokemons })
+        })
+        .catch(error => {
+          const message = `La liste des pokemons n'a pas pu etre trouvé. Réessayer dans quelques instants`
+          res.status(500).json({message, data: error})
+        })
+    }
   })
 }
