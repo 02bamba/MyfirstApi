2b0cbaeec6d505f3115b19896c583ac4d4d2a2c4
Bamba <cabkebe02@gmail.com>
2022-10-15 19:49:15 +0000

    Suite sur les validateurs,contraintes et commencement sut l'authentification

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,7 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize')
 const PokemonModel = require('../models/pokemon')
+const UserModel = require('../models/User')
 const pokemons = require('./mock-pokemon')
-  
+const bcrypt = require('bcrypt')  //encryptage de données(mdp invisible)
+
 const sequelize = new Sequelize('pokedex', 'root', 'passer', {
   host: 'localhost',
   dialect: 'mysql',
@@ -12,6 +14,7 @@ const sequelize = new Sequelize('pokedex', 'root', 'passer', {
 })
   
 const Pokemon = PokemonModel(sequelize, DataTypes)
+const User = UserModel(sequelize, DataTypes) // Istancier le model au pres de sequlize
   
 const initDb = () => {
   return sequelize.sync({force: true}).then(_ => {
@@ -22,12 +25,22 @@ const initDb = () => {
         cp: pokemon.cp,
         picture: pokemon.picture,
         types: pokemon.types.join()
-      }).then(pokemon => console.log(pokemon.toJSON()))
+      }).then(pokemon => console.log(pokemon.toJson()))
     })
+
+    bcrypt.hash('pikatchu', 10)
+    .then(hash => {
+      User.create({
+        username: 'pikatchu',
+        password: hash
+      })
+    })
+    .then(user => console.log(user.tojson()))
+
     console.log('La base de donnée a bien été initialisée !')
   })
 }
   
 module.exports = { 
-  initDb, Pokemon
+  initDb, Pokemon, User //on expose noos models pour pouvoir l'utiliser ailler dans l'API
 }
--- src/routes/findAllpokemons.js
+++ src/routes/findAllpokemons.js
@@ -6,21 +6,30 @@ module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     if (req.query.name) {
       const name = req.query.name
-      return pokemon.findAll({
+      const limit = parseInt(req.query.limit) || 5
+
+      if (name.lenght <2) {
+        const message = 'le terme recherché doit contenir au moins 2 caractères.'
+        return res.status(400).json({message})
+      }
+
+      return Pokemon.findAndCountAll({ //findAndCountAll recherche le nbrs total de resultats et les results dmdé
         where : {
           name : { // 'name' est la propriété du model pokemons
             [Op.like]: `%${name}%` //name est la critère de recherche 
             //et on fait de sort que le resultat fournit aie une/plusieurs lettre du mot fournit lors de la recherche
           }
-        }
+        },
+        order: ['name'],
+        limit: limit //La limite du nbrs de results dynamique 
       })
-      .then (pokemon => {
-        const message = `il y a ${pokemons.lenght} pokémons qui correspondent aux termes recherché: ${name}.`
-        res.json({message, data: pokemons})
+      .then (({count, rows}) => {
+        const message = `il y a ${count} pokémons qui correspondent aux termes recherché: ${name}.`
+        res.json({message, data: rows})
       })
     }
     else{
-      Pokemon.findAll()
+      Pokemon.findAll({ order: ['name'] })
         .then(pokemons => {
           const message = 'La liste des pokémons a bien été récupérée.'
           res.json({ message, data: pokemons })
